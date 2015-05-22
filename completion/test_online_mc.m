6e948625febfa64a4005182b67f2d7943f95676e
Rose Yu <yuqi.rose@gmail.com>
2015-05-21 17:09:03 -0700

    Fix the graph construction logic

--- completion/test_online_mc.m
+++ completion/test_online_mc.m
@@ -1,41 +1,64 @@
 clear; clc;
 %% gen data
-sz = [100, 100];
+n1 = 100; n2 = 200;
+sz = [n1, n2];
 X = rand(sz);
+% low rank projection
+r = 50;
+[U, S, V] = svd(X);
+S(r+1:end,r+1:end)=0;
+X = U*S*V';
 
 n = prod(sz); % total number of entries
-m = 3; % total number of blocks
-k = ceil(n * 0.8);
+k = ceil (0.65 * n );
+Omega_ind = randsample(n, k );  %observed entries
+Omega = zeros(n1,n2);
+Omega(Omega_ind) = 1;
 
+% construct adjacent matrix
 
-% simulate observed entries
-A = eye(10);
-B = ones(5);
-tmp = kron(A,B) ;
-Omega = zeros(sz);
+[I,J] = ind2sub(sz,Omega_ind);
+G = sparse([I,I+n2],[J+n1,J], 1, n1+n2, n1+n2);
+G = full(G);
 
-% non-overlapping
-Omega(1:50,51:end) = tmp;
-Omega(51:end, 1:50)= tmp;
+% simulate observed entries 
+% % non-overlapping
+% A = eye(10);
+% B = ones(5);
+% tmp = kron(A,B) ;
+% Omega = zeros(sz);
+% Omega(1:50,51:end) = tmp;
+% Omega(51:end, 1:50)= tmp;
+
+% overlapping
+% A = eye(10);
+% B = ones(8);
+% tmp = kron(A,B);
+% Omega = zeros(sz);
+% Omega(1:80,21:end) = tmp;
+% Omega(21:end,1:80) = Omega(21:end,1:80)+  tmp;
+% Omega(Omega>1) =1;
+% 
 
 %% trim degree
-r = 5;
-v = [];
-while( length(v)~= length(Omega) )
-    degree = sum(Omega);
-    v = find(degree >= r );
-    Omega = Omega( v, v);
+degree = sum(G);
+v = find(degree < r );
+while( ~isempty(v)   && ~isempty(G))
+    % remove vertex
+    G(v,:) = 0;
+    G(:,v) = 0; 
+    % check degree
+    degree = sum(G);
+    v = find(degree < r ); 
+    disp(length(v));
 end
-
 
 %% find strongly-connected component
 
-graph = sparse(Omega);
-[K,C] = graphconncomp(graph);
+graph = sparse(G);
+[K,C] = graphconncomp(graph); % BUG: too many singletons
 
 %% optimization
-n1 = sz(1);
-n2 = sz(2);
 
 df = r*(n1+n2-r);
 oversampling = 5; 
@@ -58,25 +81,50 @@ maxiter = 500;
 tol = 1e-4;
 
 X_c = zeros(sz);
+submat_idx = [];
 for i = 1:K
     blk_idx = find(C==i);
+    %ignore singleton
     n_i = length(blk_idx);
-    Omega_i=  Omega(blk_idx, blk_idx);
-    Omega_i = find(Omega_i==1); % must be linear index
-    Xi = X(blk_idx,blk_idx);
-    data_i = Xi(Omega_i);
+    if (n_i ==1)
+        continue;
+    end
+    
+    row_idx = {blk_idx(blk_idx<=n1)};
+    col_idx = {blk_idx(blk_idx>n1)-n1};
+    submat_idx = [submat_idx;row_idx, col_idx];
+end
+
+
+% optmization
+for i = 1: size(submat_idx,1)
+    row_idx =  submat_idx{i,1};
+    col_idx =  submat_idx{i,2};
+    n1_i = length(row_idx);
+    n2_i = length(col_idx);
+    Omega_i=  Omega( row_idx, col_idx);
+    Omega_i_ind = find(Omega_i==1); % must be linear index
+    Xi = X(row_idx,col_idx);
+    data_i = Xi(Omega_i_ind);
     % solve svt for each block
-    [U,S,V,numiter] = SVT(n_i,Omega_i,data_i,tau,delta,maxiter,tol);
+    [U,S,V,~] = SVT([n1_i, n2_i],Omega_i_ind,data_i,tau,delta,maxiter,tol);
     Xi_c = U*S*V';
-    X_c(blk_idx, blk_idx) = Xi_c;   
+    X_c(row_idx, col_idx) = Xi_c;   
 end
 
 %% evaluate
 fprintf('The relative recovery error is: %d\n', norm(X-X_c,'fro')/norm(X,'fro'))
 
 %% compare with full matrix completion
-Omega = find(Omega==1);
-data = X(Omega);
-[U,S,V,numiter] = SVT(sz,Omega,data,tau,delta,maxiter,tol);
-X_c2 = U*S*V'
+Omega_ind = find(Omega==1);
+data = X(Omega_ind);
+[U,S,V,numiter] = SVT(sz,Omega_ind,data,tau,delta,maxiter,tol);
+X_c2 = U*S*V';
 fprintf('The relative recovery error is: %d\n', norm(X-X_c2,'fro')/norm(X,'fro'))
+
+
+% TD: overlapp components
+% TD: compare with adaptive mc
+% TD: generalize to tensor
+
+
