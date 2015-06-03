0e9a203b675afbe3daf6525f97476e19e6d63872
Rose Yu <yuqi.rose@gmail.com>
2015-06-03 00:22:07 -0400

    Added Gandy orignal algorithm

---
+++ completion/admm_low_n_rank.m
@@ -0,0 +1,38 @@
+function [ X,Z,obj  ] = admm_low_n_rank( sz,Omega, b, lambda, rho, maxiter )
+%ADMM_LOW_N_RANK Summary of this function goes here
+%   Detailed explanation goes here
+N= 3;
+X = tenzeros(sz);
+Z = cell(1,N);
+Y = cell(1,N);
+comp_ind = [1:prod(sz)]';
+ind_2  = setdiff(comp_ind, Omega);
+
+X_old = X;
+obj = zeros(maxiter,1);
+for n = 1:N
+    Z{n} = tenzeros(sz);
+    Y{n} = tenzeros(sz);
+end
+for iter = 1: maxiter
+    fprintf('\b\b\b\b%4d',iter); 
+    tmp = tenzeros(sz);
+    for n = 1:N
+        tmp = tmp + Y{n} + rho * Z{n};
+    end
+    X(Omega) = ( tmp(Omega) + lambda * b ) ./ ( lambda + N *rho) ;
+    X(ind_2) =  tmp(ind_2)./ (N*rho);
+    for n = 1:N
+        X_n = tenmat(X,n);
+        X_n = X_n.data;
+        Y_n = tenmat(Y{n},n);
+        Y_n = Y_n.data;
+        Z{n} = refold(shrink(X_n - 1/rho * Y_n ,1/rho ), n, sz);
+        Y{n} = Y{n} + rho * (Z{n} -X );
+    end
+    obj(iter) = norm(X - X_old) / norm(X_old);
+    X_old = X;
+end
+
+end
+
--- completion/test_admm_tensor_solver.m
+++ completion/test_admm_tensor_solver.m
@@ -7,10 +7,12 @@ sub_idx = cell(1,3);
 for j = 1:3
     sub_idx{j} = 1:sz(j);
 end
-lambda = 1e-1;
-rho = 1e2;
-maxiter = 10;
+lambda = 1e-2;
+rho = 1e-3;
+maxiter = 100;
 
 
-[ Z,X,obj] = admm_tensor_solver( sz,Omega, b, sub_idx, lambda, rho, maxiter );
+% [ Z,X,obj] = admm_tensor_solver( sz,Omega, b, sub_idx, lambda, rho, maxiter );
+[ X,Z,obj] = admm_low_n_rank( sz,Omega, b, lambda, rho, maxiter );
+
 plot(obj);
